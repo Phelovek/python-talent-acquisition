f7ae54ac2221aa2b98ff2ea60f0af19e855ae81c
Sova <lisiy_07@ukr.net>
2022-04-18 15:43:40 +0300

    talent-acquisition fanal release -=)

--- main.py
+++ main.py
@@ -5,32 +5,40 @@ import csv
 
 
 def csv_reader(file):
-
     reader = csv.reader(file)
-
+    phone = []
+    email = []
+    site = []
+    data = {
+        'phone number': phone,
+        'email adress': email,
+                 'website': site}
     for row in reader:
 
         for item in row:
 
             if re.search(r'380[0-9]{9}', item) and re.search(r'\w+\.*\w+@\w+\.\w+', item):
-                phone = re.search(r'380[0-9]{9}', item).group()
-                email = re.search(r'\w+\.*\w+@\w+\.\w+', item).group()
-                sit = re.search(r'www/[A-Za-z]/[a-z]', item)
+                phon = re.search(r'380[0-9]{9}', item).group()
+                emai = re.search(r'\w+\.*\w+@\w+\.\w+', item).group()
+                si = re.search(
+                    r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:]))',
+                    item)
 
-                if sit:
-                    site = sit.group()
+                if si:
+                    sit = si.group()
                 else:
-                    site = 'None'
-                data1 = {
-                    'phone number': phone,
-                    'email adress': email,
-                    'website': site}
+                    sit = 'None'
+                phone.append(phon)
+                email.append(emai)
+                site.append(sit)
 
-                frame = pd.DataFrame(data=data1, index=[0])
+            else:
+                continue
+        frame = pd.DataFrame(data=data)
 
-                frame.to_csv(r'python-talent-acquisition/dataset1.csv', mode='a', index=False)
+        frame.to_csv(r'python-talent-acquisition/dataset_sorted.csv', index=False)
 
 
 if __name__ == '__main__':
-    with open(file=r'python-talent-acquisition/dataset.csv', mode='r', encoding='utf-8') as data:
-        csv_reader(data)
+    with open(file=r'python-talent-acquisition/dataset.csv', mode='r', encoding='utf-8') as file:
+        csv_reader(file)
